4656e50d581bbe301361bfd0cee41ebf495fe8bc
Rachel <rachel@summa.one>
2018-08-31 09:46:04 -0700

    Added setup and tox.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='riemann-keys',
+    version='0.0.1',
+    description=('Hierarchical deterministic wallet creation tool'),
+    author=[
+        "Harsha Goli",
+        "James Prestwich",
+        "RJ Rybarczyk",
+        "Jarrett Wheatley"
+        ],
+    license="LGPLv3.0",
+    install_requires=[],
+    packages=find_packages(),
+    package_dir={'riemann_keys': 'riemann_keys'},
+    package_data={'riemann_keys': ['riemann_keys/data/english.txt']},
+    keywords='bitcoin ethereum keys bip32 bip39 bip44'
+)
