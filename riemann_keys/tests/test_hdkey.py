eef6cbaf5b14ce2d9176e3b30f7316ed6bd0d096
Rachel <rachel@summa.one>
2018-09-07 14:02:50 -0700

    Riemann-keys package editable, added testing files.

---
+++ riemann_keys/tests/test_hdkey.py
@@ -0,0 +1,28 @@
+import unittest
+
+
+class TestHDKey(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_init(self):
+        pass
+
+    def test_from_entropy(self):
+        pass
+
+    def test_from_mnemonic(self):
+        pass
+
+    def test_mnemonic_to_bytes(self):
+        pass
+
+    def test_mnemonic_lookup(self):
+        pass
+
+    def test_import_word_list(self):
+        pass
+
+    def test_validate_mnemonic(self):
+        pass
---
+++ riemann_keys/tests/test_utils.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestUtils(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_pbkdf2_hmac(self):
+        pass
+
+    def test_hmac_sha512(self):
+        pass
