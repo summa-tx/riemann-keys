892b4b2d7df7c311ed1c82f2a94c0a0a92c90804
Harsha Goli <harshagoli@gmail.com>
2018-11-15 14:17:42 -0500

    examples for deriving to a node

---
+++ riemann_keys/examples/example.py
@@ -0,0 +1,18 @@
+from riemann_keys.hdkey import HDKey
+
+# Mnemonic to root
+MNEMONIC = "keen speed ice always runway film setup sentence update stove distance merge"
+root_seed = HDKey.root_seed_from_mnemonic(mnemonic=MNEMONIC)
+root = HDKey.from_root_seed(root_seed=root_seed)
+
+print("public key " + root.public_key.hex())
+print("private key " + root.private_key.hex())
+print("chaincode " + root.chain_code.hex())
+print("\n")
+
+# Derive to a node at depth of 5
+node = root.derive_path("m/44'/1'/1'/0/1")
+print("public key " + node.public_key.hex())
+print("private key " + node.private_key.hex())
+print("chaincode " + node.chain_code.hex())
+print("depth " + str(node.depth))
