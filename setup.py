afc58fb55cffb52866ba9792fcb39e703f0a6d06
James Prestwich <james@prestwi.ch>
2019-10-18 17:31:30 -0700

    chore: cleanup setup

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
+# flake8: noqa
+
 from setuptools import setup, find_packages
+
+authors = [
+    "Harsha Goli",
+    "James Prestwich",
+    "RJ Rybarczyk",
+    "Jarrett Wheatley"
+]
 
 setup(
     name='riemann-keys',
     version='0.1.2',
+    url='https://github.com/summa-tx/riemann-keys',
     description=('Hierarchical deterministic wallet creation tool'),
-    author=[
-        "Harsha Goli",
-        "James Prestwich",
-        "RJ Rybarczyk",
-        "Jarrett Wheatley"
-        ],
-    license="LGPLv3.0",
+    author=authors,
+    author_email='james@summa.one',
     install_requires=['riemann-secpy256k1==0.2.8'],
     packages=find_packages(),
     package_dir={'riemann_keys': 'riemann_keys'},
-    keywords='bitcoin ethereum keys bip32 bip39 bip44'
+    keywords='bitcoin ethereum keys bip32 bip39 bip44',
+    classifiers=[
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)'
+    ]
 )
